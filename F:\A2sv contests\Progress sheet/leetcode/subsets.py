14b07b4051581f17b1aeeed77f5096e121d3f52e
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2024-03-05 15:12:32 +0300

    Add solution for Subsets

--- F:\A2sv contests\Progress sheet/leetcode/subsets.py
+++ F:\A2sv contests\Progress sheet/leetcode/subsets.py
@@ -1,19 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        
         def backtrack(idx, sub):
-            # base case
-            if len(nums) < idx:
+            if idx > len(nums): #maximum subarry length = length(nums)
                 return
+            
             ans.append(sub[:])
-            
-            for i in range(idx, len(nums)):
-                sub.append(nums[i])
-                backtrack(i+1, sub)
-                sub.pop()
 
-            
+            for i in range(idx, len(nums)):
+                backtrack(i+1, sub + [nums[i]])
+
         backtrack(0, [])
         return ans
 
