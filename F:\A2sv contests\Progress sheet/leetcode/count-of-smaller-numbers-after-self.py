1d80458f8479941cfe10c57a54ee013ee2bcfa1d
Yetnayet Lakew <127979977+infangle@users.noreply.github.com>
2024-03-23 23:12:26 +0300

    Add solution for Count of Smaller Numbers After Self

---
+++ F:\A2sv contests\Progress sheet/leetcode/count-of-smaller-numbers-after-self.py
@@ -0,0 +1,12 @@
+from sortedcontainers import SortedList
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        s = SortedList()
+        output = []
+
+        for n in nums[::-1]:
+            ans = SortedList.bisect_left(s, n)
+            output.append(ans)
+            s.add(n)
+
+        return reversed(output)
